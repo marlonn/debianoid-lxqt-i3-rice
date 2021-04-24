791264f10df7c022aae8ae5127f1bae8b3f65fb0
marlonn <19227239+marlonn@users.noreply.github.com>
2021-04-24 13:02:40 +0000

    Update cp.sh

--- cp.sh
+++ cp.sh
@@ -2,3 +2,4 @@ cp .config/ranger/rc.conf ~/.config/ranger/rc.conf
 cp .config/i3/config ~/.config/i3/config 
 cp .config/micro/settings.json ~/.config/micro/settings.json
 cp .config/gtk-3.0/gtk.css ~/.config/gtk-3.0/gtk.css
+cp .config/lxterminal/lxterminal.conf ~/.config/lxterminal/lxterminal.conf
