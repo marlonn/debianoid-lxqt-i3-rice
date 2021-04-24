7d99b108c660ab4de49025565b08eeead29d4c58
marlonn <19227239+marlonn@users.noreply.github.com>
2021-04-24 13:12:44 +0000

    Update cp.sh

--- cp.sh
+++ cp.sh
@@ -3,3 +3,4 @@ cp .config/i3/config ~/.config/i3/config
 cp .config/micro/settings.json ~/.config/micro/settings.json
 cp .config/gtk-3.0/gtk.css ~/.config/gtk-3.0/gtk.css
 cp .config/lxterminal/lxterminal.conf ~/.config/lxterminal/lxterminal.conf
+cp .selected_editor ~/
