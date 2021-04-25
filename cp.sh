3ffa86e04918fb29185c84851e69bb77ab6bb1f5
marlonn <19227239+marlonn@users.noreply.github.com>
2021-04-25 01:08:06 +0000

    Update cp.sh

--- cp.sh
+++ cp.sh
@@ -1,4 +1,4 @@
-mkdir ~/.config/ranger ~/.config/i3 ~/.config/micro ~/.config/lxterminal
+mkdir ~/.config/ranger ~/.config/i3 ~/.config/micro ~/.config/lxterminal ~/.config/gtk-3.0
 cp .config/ranger/rc.conf ~/.config/ranger/rc.conf
 cp .config/i3/config ~/.config/i3/config 
 cp .config/micro/settings.json ~/.config/micro/settings.json
